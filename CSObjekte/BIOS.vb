f5d4524431ff76283a94cebc2d1d74f186eb8833
Christian Schmidt <47923480+chrisschm@users.noreply.github.com>
2019-03-13 13:30:03 +0100

    additions to BIOS class

--- CSObjekte/BIOS.vb
+++ CSObjekte/BIOS.vb
@@ -8,7 +8,7 @@
                 ' Private Variables to store Attributes of Win32_BIOS WMI Object
                 ' Documentation: https://docs.microsoft.com/en-us/windows/desktop/cimwin32prov/win32-bios
 
-                Private srtBiosCharacteristics As UShort()
+                Private strucBiosCharacteristics As BiosCharacteristicValues
                 Private strBiosName As String
                 Private strBIOSVersion As String()
                 Private strBuildNumber As String
@@ -44,6 +44,77 @@
 
 #End Region
 
+#Region "PublicStrutures"
+                Public Structure BiosCharacteristicValues
+                    Public Reserved0 As Boolean
+                    Public Reserved1 As Boolean
+                    Public Unknown2 As Boolean
+                    Public BIOSCharacteristicsNotSupported As Boolean
+                    Public ISASupported As Boolean
+                    Public MCASupported As Boolean
+                    Public EISASupported As Boolean
+                    Public PCISupported As Boolean
+                    Public PCCardSupported As Boolean
+                    Public PlugNPlaySupported As Boolean
+                    Public APMSupported As Boolean
+                    Public BIOSUpgradeable As Boolean
+                    Public BIOSShadowingAllowed As Boolean
+                    Public VLVESASupported As Boolean
+                    Public ESCDSupportAvailable As Boolean
+                    Public BootFromCDSupported As Boolean
+                    Public SelectableBootSupported As Boolean
+                    Public BIOSROMSocketed As Boolean
+                    Public BootFromPCCardSupported As Boolean
+                    Public EDDSpecificationSupported As Boolean
+                    Public Int13hFloppyNEC9800Supported As Boolean
+                    Public Int13hFloppyToshibaSupported As Boolean
+                    Public Int13h360KBFloppyServicesSupported As Boolean
+                    Public Int13h12MBFloppyServicesSupported As Boolean
+                    Public Int13h720KBFloppyServicesSupported As Boolean
+                    Public Int13h288MBFloppyServicesSupported As Boolean
+                    Public Int5hPrintScreenServiceSupported As Boolean
+                    Public Int9h8042KeyboardServicesSupported As Boolean
+                    Public Int14hSerialServicesSupported As Boolean
+                    Public Int17hPrinterServicesSupported As Boolean
+                    Public Int10hCGAMonoVideoServicesSupported As Boolean
+                    Public NECPC98 As Boolean
+                    Public ACPISupported As Boolean
+                    Public USBLegacySupported As Boolean
+                    Public AGPSupported As Boolean
+                    Public I2OBootSupported As Boolean
+                    Public LS120BootSupported As Boolean
+                    Public ATAPIZIPDriveBootSupported As Boolean
+                    Public FW1394BootSupported As Boolean
+                    Public SmartBatterySupported As Boolean
+                    Public BIOSVendorReserved40 As Boolean
+                    Public BIOSVendorReserved41 As Boolean
+                    Public BIOSVendorReserved42 As Boolean
+                    Public BIOSVendorReserved43 As Boolean
+                    Public BIOSVendorReserved44 As Boolean
+                    Public BIOSVendorReserved45 As Boolean
+                    Public BIOSVendorReserved46 As Boolean
+                    Public BIOSVendorReserved47 As Boolean
+                    Public SystemVendorReserved48 As Boolean
+                    Public SystemVendorReserved49 As Boolean
+                    Public SystemVendorReserved50 As Boolean
+                    Public SystemVendorReserved51 As Boolean
+                    Public SystemVendorReserved52 As Boolean
+                    Public SystemVendorReserved53 As Boolean
+                    Public SystemVendorReserved54 As Boolean
+                    Public SystemVendorReserved55 As Boolean
+                    Public SystemVendorReserved56 As Boolean
+                    Public SystemVendorReserved57 As Boolean
+                    Public SystemVendorReserved58 As Boolean
+                    Public SystemVendorReserved59 As Boolean
+                    Public SystemVendorReserved60 As Boolean
+                    Public SystemVendorReserved61 As Boolean
+                    Public SystemVendorReserved62 As Boolean
+                    Public SystemVendorReserved63 As Boolean
+                End Structure
+#End Region
+
+
+
                 Public Sub New()
 
                     Dim i As Integer
@@ -57,7 +128,9 @@
                         Case 1
                             Dim mosB As Global.System.Management.ManagementObject = mosBs.[Get](0)
 
-                            srtBiosCharacteristics = mosB("BiosCharacteristics")
+                            InitBiosCharacteristicValues()
+                            strucBiosCharacteristics = GetBiosCharacteristicValues(mosB("BiosCharacteristics"))
+
                             strBiosName = mosB("Name")
                             strBIOSVersion = mosB("BIOSVersion")
                             strBuildNumber = mosB("BuildNumber")
@@ -93,11 +166,223 @@
 
                 End Sub
 
+#Region "PrivateMethods"
+                Private Function GetBiosCharacteristicValues(Values As UShort()) As BiosCharacteristicValues
+
+                    For Each value As UShort In Values
+                        Select Case value
+                            Case 0
+                                strucBiosCharacteristics.Reserved0 = True
+                            Case 1
+                                strucBiosCharacteristics.Reserved1 = True
+                            Case 2
+                                strucBiosCharacteristics.Unknown2 = True
+                            Case 3
+                                strucBiosCharacteristics.BIOSCharacteristicsNotSupported = True
+                            Case 4
+                                strucBiosCharacteristics.ISASupported = True
+                            Case 5
+                                strucBiosCharacteristics.MCASupported = True
+                            Case 6
+                                strucBiosCharacteristics.EISASupported = True
+                            Case 7
+                                strucBiosCharacteristics.PCISupported = True
+                            Case 8
+                                strucBiosCharacteristics.PCCardSupported = True
+                            Case 9
+                                strucBiosCharacteristics.PlugNPlaySupported = True
+                            Case 10
+                                strucBiosCharacteristics.APMSupported = True
+                            Case 11
+                                strucBiosCharacteristics.BIOSUpgradeable = True
+                            Case 12
+                                strucBiosCharacteristics.BIOSShadowingAllowed = True
+                            Case 13
+                                strucBiosCharacteristics.VLVESASupported = True
+                            Case 14
+                                strucBiosCharacteristics.ESCDSupportAvailable = True
+                            Case 15
+                                strucBiosCharacteristics.BootFromCDSupported = True
+                            Case 16
+                                strucBiosCharacteristics.SelectableBootSupported = True
+                            Case 17
+                                strucBiosCharacteristics.BIOSROMSocketed = True
+                            Case 18
+                                strucBiosCharacteristics.BootFromPCCardSupported = True
+                            Case 19
+                                strucBiosCharacteristics.EDDSpecificationSupported = True
+                            Case 20
+                                strucBiosCharacteristics.Int13hFloppyNEC9800Supported = True
+                            Case 21
+                                strucBiosCharacteristics.Int13hFloppyToshibaSupported = True
+                            Case 22
+                                strucBiosCharacteristics.Int13h360KBFloppyServicesSupported = True
+                            Case 23
+                                strucBiosCharacteristics.Int13h12MBFloppyServicesSupported = True
+                            Case 24
+                                strucBiosCharacteristics.Int13h720KBFloppyServicesSupported = True
+                            Case 25
+                                strucBiosCharacteristics.Int13h288MBFloppyServicesSupported = True
+                            Case 26
+                                strucBiosCharacteristics.Int5hPrintScreenServiceSupported = True
+                            Case 27
+                                strucBiosCharacteristics.Int9h8042KeyboardServicesSupported = True
+                            Case 28
+                                strucBiosCharacteristics.Int14hSerialServicesSupported = True
+                            Case 29
+                                strucBiosCharacteristics.Int17hPrinterServicesSupported = True
+                            Case 30
+                                strucBiosCharacteristics.Int10hCGAMonoVideoServicesSupported = True
+                            Case 31
+                                strucBiosCharacteristics.NECPC98 = True
+                            Case 32
+                                strucBiosCharacteristics.ACPISupported = True
+                            Case 33
+                                strucBiosCharacteristics.USBLegacySupported = True
+                            Case 34
+                                strucBiosCharacteristics.AGPSupported = True
+                            Case 35
+                                strucBiosCharacteristics.I2OBootSupported = True
+                            Case 36
+                                strucBiosCharacteristics.LS120BootSupported = True
+                            Case 37
+                                strucBiosCharacteristics.ATAPIZIPDriveBootSupported = True
+                            Case 38
+                                strucBiosCharacteristics.FW1394BootSupported = True
+                            Case 39
+                                strucBiosCharacteristics.SmartBatterySupported = True
+                            Case 40
+                                strucBiosCharacteristics.BIOSVendorReserved40 = True
+                            Case 41
+                                strucBiosCharacteristics.BIOSVendorReserved41 = True
+                            Case 42
+                                strucBiosCharacteristics.BIOSVendorReserved42 = True
+                            Case 43
+                                strucBiosCharacteristics.BIOSVendorReserved43 = True
+                            Case 44
+                                strucBiosCharacteristics.BIOSVendorReserved44 = True
+                            Case 45
+                                strucBiosCharacteristics.BIOSVendorReserved45 = True
+                            Case 46
+                                strucBiosCharacteristics.BIOSVendorReserved46 = True
+                            Case 47
+                                strucBiosCharacteristics.BIOSVendorReserved47 = True
+                            Case 48
+                                strucBiosCharacteristics.SystemVendorReserved48 = True
+                            Case 49
+                                strucBiosCharacteristics.SystemVendorReserved49 = True
+                            Case 50
+                                strucBiosCharacteristics.SystemVendorReserved50 = True
+                            Case 51
+                                strucBiosCharacteristics.SystemVendorReserved51 = True
+                            Case 52
+                                strucBiosCharacteristics.SystemVendorReserved52 = True
+                            Case 53
+                                strucBiosCharacteristics.SystemVendorReserved53 = True
+                            Case 54
+                                strucBiosCharacteristics.SystemVendorReserved54 = True
+                            Case 55
+                                strucBiosCharacteristics.SystemVendorReserved55 = True
+                            Case 56
+                                strucBiosCharacteristics.SystemVendorReserved56 = True
+                            Case 57
+                                strucBiosCharacteristics.SystemVendorReserved57 = True
+                            Case 58
+                                strucBiosCharacteristics.SystemVendorReserved58 = True
+                            Case 59
+                                strucBiosCharacteristics.SystemVendorReserved59 = True
+                            Case 60
+                                strucBiosCharacteristics.SystemVendorReserved60 = True
+                            Case 61
+                                strucBiosCharacteristics.SystemVendorReserved61 = True
+                            Case 62
+                                strucBiosCharacteristics.SystemVendorReserved62 = True
+                            Case 63
+                                strucBiosCharacteristics.SystemVendorReserved63 = True
+                            Case Else
+                                Throw New Exception("Unknown Value in BiosCharacteristics")
+                        End Select
+                    Next
+
+                End Function
+
+                Private Sub InitBiosCharacteristicValues()
+
+                    With strucBiosCharacteristics
+                        .Reserved0 = False
+                        .Reserved1 = False
+                        .Unknown2 = False
+                        .BIOSCharacteristicsNotSupported = False
+                        .ISASupported = False
+                        .MCASupported = False
+                        .EISASupported = False
+                        .PCISupported = False
+                        .PCCardSupported = False
+                        .PlugNPlaySupported = False
+                        .APMSupported = False
+                        .BIOSUpgradeable = False
+                        .BIOSShadowingAllowed = False
+                        .VLVESASupported = False
+                        .ESCDSupportAvailable = False
+                        .BootFromCDSupported = False
+                        .SelectableBootSupported = False
+                        .BIOSROMSocketed = False
+                        .BootFromPCCardSupported = False
+                        .EDDSpecificationSupported = False
+                        .Int13hFloppyNEC9800Supported = False
+                        .Int13hFloppyToshibaSupported = False
+                        .Int13h360KBFloppyServicesSupported = False
+                        .Int13h12MBFloppyServicesSupported = False
+                        .Int13h720KBFloppyServicesSupported = False
+                        .Int13h288MBFloppyServicesSupported = False
+                        .Int5hPrintScreenServiceSupported = False
+                        .Int9h8042KeyboardServicesSupported = False
+                        .Int14hSerialServicesSupported = False
+                        .Int17hPrinterServicesSupported = False
+                        .Int10hCGAMonoVideoServicesSupported = False
+                        .NECPC98 = False
+                        .ACPISupported = False
+                        .USBLegacySupported = False
+                        .AGPSupported = False
+                        .I2OBootSupported = False
+                        .LS120BootSupported = False
+                        .ATAPIZIPDriveBootSupported = False
+                        .FW1394BootSupported = False
+                        .SmartBatterySupported = False
+                        .BIOSVendorReserved40 = False
+                        .BIOSVendorReserved41 = False
+                        .BIOSVendorReserved42 = False
+                        .BIOSVendorReserved43 = False
+                        .BIOSVendorReserved44 = False
+                        .BIOSVendorReserved45 = False
+                        .BIOSVendorReserved46 = False
+                        .BIOSVendorReserved47 = False
+                        .SystemVendorReserved48 = False
+                        .SystemVendorReserved49 = False
+                        .SystemVendorReserved50 = False
+                        .SystemVendorReserved51 = False
+                        .SystemVendorReserved52 = False
+                        .SystemVendorReserved53 = False
+                        .SystemVendorReserved54 = False
+                        .SystemVendorReserved55 = False
+                        .SystemVendorReserved56 = False
+                        .SystemVendorReserved57 = False
+                        .SystemVendorReserved58 = False
+                        .SystemVendorReserved59 = False
+                        .SystemVendorReserved60 = False
+                        .SystemVendorReserved61 = False
+                        .SystemVendorReserved62 = False
+                        .SystemVendorReserved63 = False
+                    End With
+
+                End Sub
+#End Region
+
 #Region "PublicProperties"
 
-                Public ReadOnly Property BiosCharacteristics() As UShort()
+                Public ReadOnly Property BiosCharacteristics() As BiosCharacteristicValues
                     Get
-                        Return srtBiosCharacteristics
+                        Return strucBiosCharacteristics
                     End Get
                 End Property
 
